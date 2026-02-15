3aad75c0b4814cf606cea846d65eebde2ba9dc69
voidtrace <voidtrace_sigma@proton.me>
2026-02-15 09:13:39 +0800

    script to manage the survey data

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const _ = require('lodash');
+const bibtexParse = require('bibtex-parse-js');
+const Handlebars = require("handlebars");
+
+
+const TOOLS_TMPL = "./templates/tools.hbs", RESEARCH_TMPL = "./templates/research.hbs";
+
+const condense_bib_list = (bib_entries) => _.map(
+    bib_entries, (entry) => ({ ...{ handle:entry.citationKey, type:entry.entryType }, ...entry.entryTags })
+); 
+
+const end2end_papers = condense_bib_list(bibtexParse.toJSON(fs.readFileSync("./references/end2end.bib", "utf-8")) ),
+      benchmark_papers = condense_bib_list(bibtexParse.toJSON(fs.readFileSync("./references/benchmarks.bib", "utf-8")) ),
+      surveys_papers = condense_bib_list(bibtexParse.toJSON(fs.readFileSync("./references/surveys.bib", "utf-8")) );
+
+const framework_sw = condense_bib_list( bibtexParse.toJSON(fs.readFileSync("./references/framework-gh.bib", "utf-8")) );
+
+
+
+
+const gen_doc_tools = () => {
+    const tmpl_str = fs.readFileSync(TOOLS_TMPL,"utf-8");
+    const tmpl = Handlebars.compile(tmpl_str), list = {projects: framework_sw };
+    
+    const res = tmpl( list );
+    console.log(res);
+}
+
+const gen_doc_research = () => {
+    const tmpl_str = fs.readFileSync(RESEARCH_TMPL,"utf-8");
+    const tmpl = Handlebars.compile(tmpl_str), list = { 
+        surveys: surveys_papers,
+        e2e: _.sortBy(end2end_papers,'year').reverse(),
+        benchmarks: _.sortBy(benchmark_papers,'year').reverse()
+    };
+    
+    const res = tmpl( list );
+    console.log(res);
+}
+
+
+(async function () {
+    // gen_doc_tools();
+    gen_doc_research();
+
+    // console.log(surveys_papers);
+
+})();
